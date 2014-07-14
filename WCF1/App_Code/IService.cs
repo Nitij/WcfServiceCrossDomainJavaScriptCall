b91b6a55e3d86a1ff4cb61fff0580c9e1597ae17
Nitij <nitij.jp@gmail.com>
2014-07-14 22:47:22 +0530

    Initial commit.

---
+++ WCF1/App_Code/IService.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.ServiceModel.Activation;
+using System.Text;
+
+namespace Microsoft.ServiceModel.Samples
+{
+    [ServiceContract(Namespace = "")]
+    
+    public interface ICalculator
+    {
+        /// <summary>
+        /// Add
+        /// </summary>
+        /// <param name="n1"></param>
+        /// <param name="n2"></param>
+        /// <returns></returns>
+        [WebInvoke(
+        Method = "*",
+        RequestFormat = WebMessageFormat.Json,
+        ResponseFormat = WebMessageFormat.Json,
+        BodyStyle = WebMessageBodyStyle.WrappedRequest)]
+        [OperationContract]
+        Double Add(Double n1, Double n2);
+
+        /// <summary>
+        /// Subtract
+        /// </summary>
+        /// <param name="n1"></param>
+        /// <param name="n2"></param>
+        /// <returns></returns>
+        [OperationContract]
+        [WebInvoke(
+        Method = "*",
+        RequestFormat = WebMessageFormat.Json,
+        ResponseFormat = WebMessageFormat.Json,
+        BodyStyle = WebMessageBodyStyle.WrappedRequest)]
+        Double Subtract(Double n1, Double n2);
+
+        /// <summary>
+        /// Divide
+        /// </summary>
+        /// <param name="n1"></param>
+        /// <param name="n2"></param>
+        /// <returns></returns>
+        [OperationContract]
+        [WebInvoke(
+        Method = "*",
+        RequestFormat = WebMessageFormat.Json,
+        ResponseFormat = WebMessageFormat.Json,
+        BodyStyle = WebMessageBodyStyle.WrappedRequest)]
+        Double Divide(Double n1, Double n2);
+
+        /// <summary>
+        /// Multiply
+        /// </summary>
+        /// <param name="n1"></param>
+        /// <param name="n2"></param>
+        /// <returns></returns>
+        [OperationContract]
+        [WebInvoke(
+        Method = "*",
+        RequestFormat = WebMessageFormat.Json,
+        ResponseFormat = WebMessageFormat.Json,
+        BodyStyle = WebMessageBodyStyle.WrappedRequest)]
+        Double Multiply(Double n1, Double n2);
+    }
+}
+
+
+
+
+
---
+++ WCF1/App_Code/Service.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.ServiceModel.Activation;
+using System.Text;
+
+namespace Microsoft.ServiceModel.Samples
+{
+    [AspNetCompatibilityRequirements(RequirementsMode = AspNetCompatibilityRequirementsMode.Allowed)]
+    public class CalculatorService : ICalculator
+    {
+        /// <summary>
+        /// Add two numbers
+        /// </summary>
+        /// <param name="n1"></param>
+        /// <param name="n2"></param>
+        /// <returns></returns>
+        public Double Add(Double n1, Double n2)
+        {
+            Double result = n1 + n2;
+
+            //Write to the console
+            Console.WriteLine("Recieved Add({0} + {1})", n1, n2);
+            Console.WriteLine("Result = {0}", result);
+
+            return result;
+        }
+
+        /// <summary>
+        /// Subtract one number from the other
+        /// </summary>
+        /// <param name="n1"></param>
+        /// <param name="n2"></param>
+        /// <returns></returns>
+        public Double Subtract(Double n1, Double n2)
+        {
+            Double result = n1 - n2;
+
+            //Write to the console
+            Console.WriteLine("Recieved Subtract({0} - {1})", n1, n2);
+            Console.WriteLine("Result = {0}", result);
+
+            return result;
+        }
+
+        /// <summary>
+        /// Multiply two numbers
+        /// </summary>
+        /// <param name="n1"></param>
+        /// <param name="n2"></param>
+        /// <returns></returns>
+        public Double Multiply(Double n1, Double n2)
+        {
+            Double result = n1 * n2;
+
+            //Write to the console
+            Console.WriteLine("Recieved Multiply({0} * {1})", n1, n2);
+            Console.WriteLine("Result = {0}", result);
+
+            return result;
+        }
+
+        /// <summary>
+        /// Divide two numbers
+        /// </summary>
+        /// <param name="n1"></param>
+        /// <param name="n2"></param>
+        /// <returns></returns>
+        public Double Divide(Double n1, Double n2)
+        {
+            Double result = n1 / n2;
+
+            //Write to the console
+            Console.WriteLine("Recieved Divide({0} / {1})", n1, n2);
+            Console.WriteLine("Result = {0}", result);
+
+            return result;
+        }
+    }
+}
+
+
